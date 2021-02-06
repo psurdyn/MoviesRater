71607c4afd3655fd6a284d6be9dbb778497ebe91
piosur0608 <piotrsurdyn@gmail.com>
2021-02-06 16:00:28 +0100

    Add a view model

---
+++ ViewModels/MovieRatingVM.cs
@@ -0,0 +1,14 @@
+﻿using MoviesRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesRater.ViewModels
+{
+    public class MovieRatingVM
+    {
+        public MovieModel Movie { get; set; }
+        public List<MovieRatingModel> Rating { get; set; }
+    }
+}
