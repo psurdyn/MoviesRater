a33c86ffe7e673294fd4ec4b27c71e1e1f008f6e
piosur0608 <piotrsurdyn@gmail.com>
2021-02-06 16:00:18 +0100

    Add http clients

---
+++ HttpMoviesClient/HttpMoviesClient.cs
@@ -0,0 +1,39 @@
+﻿using MoviesRater.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MoviesRater.HttpMovies
+{
+    public class HttpMoviesClient : IHttpMoviesClient
+    {
+        private HttpClient _httpClient;
+        private readonly string BasicUrlAddress = "https://swapi.dev/api/films/";
+
+        public HttpMoviesClient()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<IList<MovieModel>> GetAll()
+        {
+            var httpResponse = await _httpClient.GetAsync(BasicUrlAddress);
+            var stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            var allMovies = JsonConvert.DeserializeObject<ResultsModel>(stringResponse)?.Movies;
+
+            return allMovies;
+        }
+
+        public async Task<MovieModel> GetMovieById(int id)
+        {
+            var httpResponse = await _httpClient.GetAsync($"{BasicUrlAddress}{id}/");
+            var stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            var movie = JsonConvert.DeserializeObject<MovieModel>(stringResponse);
+
+            return movie;
+        }
+    }
+}
---
+++ HttpMoviesClient/IHttpMoviesClient.cs
@@ -0,0 +1,14 @@
+﻿using MoviesRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesRater.HttpMovies
+{
+    public interface IHttpMoviesClient
+    {
+        public Task<IList<MovieModel>> GetAll();
+        public Task<MovieModel> GetMovieById(int id);
+    }
+}
