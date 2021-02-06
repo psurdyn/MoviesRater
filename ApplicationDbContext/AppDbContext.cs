ae5739eb6475680def2c8b1de596f437960d5cf6
piosur0608 <piotrsurdyn@gmail.com>
2021-02-06 15:59:57 +0100

    Add an app db context

---
+++ ApplicationDbContext/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesRater.ApplicationDbContext
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+        DbSet<MovieRatingModel> MovieRatings { get; set; }
+        DbSet<RatingModel> Ratings { get; set; }
+
+    }
+}
