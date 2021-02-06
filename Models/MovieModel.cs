faff84b6e99b2110bcb7c8c589910250a537a51b
piosur0608 <piotrsurdyn@gmail.com>
2021-02-06 15:59:45 +0100

    Add needed models

---
+++ Models/MovieModel.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesRater.Models
+{
+    public class MovieModel
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("opening_crawl")]
+        public string OpeningCrawl { get; set; }
+        [JsonProperty("director")]
+        public string Director { get; set; }
+        [JsonProperty("producer")]
+        public string Producer { get; set; }
+        [JsonProperty("release_date")]
+        public DateTime ReleaseDate { get; set; }
+    }
+
+    public class ResultsModel
+    {
+        [JsonProperty("results")]
+        public List<MovieModel> Movies { get; set; }
+    }
+}
---
+++ Models/MovieRatingModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesRater.Models
+{
+    [Table("Ratings", Schema = "dbo")]
+    public class MovieRatingModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public RatingModel Rating { get; set; }
+        [ForeignKey("Rating")]
+        public int RatingId { get; set; }
+    }
+}
